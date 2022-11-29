748b2d82a2b0612eef0f2db80d63be2530ae6f2f
anjanshrestha8 <anjanshrestha2002@gmail.com>
2022-11-29 09:14:10 +0545

    added a folder proj

--- proj/proj/Form1.Designer.cs
+++ proj/proj/Form1.Designer.cs
@@ -30,6 +30,13 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Display = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Display)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -43,23 +50,81 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(563, 216);
+            this.button1.Location = new System.Drawing.Point(12, 87);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(61, 29);
+            this.button1.Size = new System.Drawing.Size(55, 37);
             this.button1.TabIndex = 1;
-            this.button1.Text = "click";
+            this.button1.Text = "Insert";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 46);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(55, 35);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 130);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(55, 30);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Display
+            // 
+            this.Display.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Display.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.Display.Location = new System.Drawing.Point(338, 49);
+            this.Display.Name = "Display";
+            this.Display.RowTemplate.Height = 25;
+            this.Display.Size = new System.Drawing.Size(415, 361);
+            this.Display.TabIndex = 4;
+            this.Display.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Customer_Id";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Customer_Name";
+            this.Column2.Name = "Column2";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 166);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(55, 31);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "show";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.Display);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Display)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -69,5 +134,11 @@
 
         private Label label1;
         private Button button1;
+        private Button button2;
+        private Button button3;
+        private DataGridView Display;
+        private Button button4;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
     }
 }
--- proj/proj/Form1.cs
+++ proj/proj/Form1.cs
@@ -23,9 +23,10 @@ namespace proj
                 {
                     con.Open();
                     string query = "Insert into RESTRO values('2','Burger','200')";
+              
                     SqlCommand cmd = new SqlCommand(query, con);
                     cmd.ExecuteNonQuery();
-                    MessageBox.Show("Saved Sucessful");
+                    MessageBox.Show("Insert Sucessful");
                     con.Close();
                 }
             catch(Exception ex)
@@ -35,6 +36,64 @@ namespace proj
 
          
         }
-       
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                string query = "update RESTRO set MENU_ITEM = 'MOM02' WHERE ID=2";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Update Sucessful");
+                con.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error:" + ex.InnerException);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                string query = "delete  from RESTRO WHERE ID =2";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Delete Sucessful.");
+                con.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error:" + ex.InnerException);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string[] row = new string[] { "1", "Anish" };
+            Display.Rows.Add(row);
+            row = new string[] { "2", "Anjan" };
+            Display.Rows.Add(row);
+            row = new string[] { "3", "pUKar" };
+           Display.Rows.Add(row);
+            row = new string[] { "4", "ram" };
+            Display.Rows.Add(row);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+          
+            {
+                con.Open();
+                string query = "insert into RESTRO values'1','anjan'";
+                SqlCommand cmd = new SqlCommand(query,con);
+                MessageBox.Show("Data Shown");
+                con.Close();
+            }
+          
+        }
     }
 }
